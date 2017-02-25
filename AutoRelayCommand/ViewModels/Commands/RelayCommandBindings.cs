d39eb7265bd941f8b037c7634f41a9a63ab95359
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-02-24 23:04:47 -0500

    Initial Commit

---
+++ AutoRelayCommand/ViewModels/Commands/RelayCommandBindings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace AutoRelayCommand.ViewModels.Commands
+{
+    internal class RelayCommandBindings
+    {
+        public Action<object> Execute { get; set; }
+        public IEnumerable<Predicate<object>> CanExecuteChecks { get; set; }
+        public INotifyPropertyChanged BindingModel { get; set; }
+        public IEnumerable<string> BindingProperties { get; set; }
+    }
+}
